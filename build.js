3b090a584f3ae28fbaec17728d59b4f58f4d236e
unknown <tarunsingh78490@gmail.com>
2025-09-25 04:40:52 +0530

    fixed 404 error

--- build.js
+++ build.js
@@ -1,4 +1,36 @@
-// build.js
+// // build.js
+// const esbuild = require("esbuild");
+// const path = require("path");
+// const fs = require("fs");
+
+// const inputFile = path.resolve(__dirname, "node_modules/@erc7824/nitrolite/dist/index.js");
+// const outFile = path.resolve(__dirname, "Runtime/webgl/nitrolite.bundle.js");
+// const buildOutFile = path.resolve(__dirname, "../../../Build/nitrolite.bundle.js");
+
+// esbuild.build({
+//   entryPoints: [inputFile],
+//   bundle: true,
+//   minify: true,
+//   globalName: "NitroLite",       // exposes NitroLite globally
+//   outfile: outFile,
+//   platform: "browser",
+// })
+// .then(() => {
+//   console.log("Nitrolite bundle created at", outFile);
+  
+//   // Copy to Build folder if it exists
+//   if (fs.existsSync(path.dirname(buildOutFile))) {
+//     fs.copyFileSync(outFile, buildOutFile);
+//     console.log("Copied bundle to Build folder");
+//   }
+// })
+// .catch(err => {
+//   console.error("Error bundling NitroLite:", err);
+//   process.exit(1);
+// });
+
+
+
 const esbuild = require("esbuild");
 const path = require("path");
 const fs = require("fs");
@@ -12,6 +44,8 @@ esbuild.build({
   bundle: true,
   minify: true,
   globalName: "NitroLite",       // exposes NitroLite globally
+  format: "iife",                // important: pure browser global
+  target: ["es6"],               // ensure modern JS
   outfile: outFile,
   platform: "browser",
 })
@@ -28,3 +62,4 @@ esbuild.build({
   console.error("Error bundling NitroLite:", err);
   process.exit(1);
 });
+
